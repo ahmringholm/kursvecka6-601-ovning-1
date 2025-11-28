3c2422c882c5d445dcd599090a2ab41bc62ae4f1
ahmringholm <ringholmahm4@gmail.com>
2025-11-28 13:32:44 +0100

    added code to css and js files

---
+++ main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("moreInfoText");
+const text = document.getElementById("moreInfoBtn");
+
+btn.addEventListener("click", () => {
+    text.textContent = "Katt är vanligt att ha som husdjur. De är mysiga och har roliga personligheter.";
+});
